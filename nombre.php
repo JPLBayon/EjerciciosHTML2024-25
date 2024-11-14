6d41db45b2a12ac42964fd3d8eb77de090eb0dd7
Julio Pedro López Bayón <juliolb@educastur.org>
2024-11-14 16:50:05 +0100

    Actualización

---
+++ nombre.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Formulario Nombre</title>
+	<meta charset="utf-8"  />
+</head>
+
+<body> 
+    <h1><?php echo "Hola " . $_GET["nombre"] ?></h1>
+</body>
+</html>
